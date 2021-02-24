aa32fd7d6a638166c88c771125db79683d6d8b87
morsesolutions <morsesolutions@outlook.com>
2021-02-24 12:07:22 -0500

    added secret to code

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -12,6 +12,8 @@ namespace PSWebApp.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        private string sasToken = "https://nmstgacct1.blob.core.windows.net/container1?sp=r&st=2021-02-24T17:06:44Z&se=2021-02-25T01:06:44Z&spr=https&sv=2020-02-10&sr=c&sig=CND75W9WltU00B9Xoy7zCs4JRtrIKUHu4X%2Ftk8biNhQ%3D";
+
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
